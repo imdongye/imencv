1f83388f75e193534fe0b6162cda57dcfb08eb69
imdongye <imdongye@naver.com>
2026-02-10 23:58:08 +0900

    done build opencv

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int, char**){
-    std::cout << "Hello, from imencv!\n";
+    std::cout << "======== OpenCV Build Information ========" << std::endl;
+    std::cout << cv::getBuildInformation() << std::endl;
+    std::cout << "===========================================" << std::endl;
+    std::cout << "Hello, hi imencv!\n";
 }
